cda7a35eacc65bd9eeb26dc83a1cf3b45629443e
André Lucas <contact@andrepaiva.dev>
2024-09-14 13:25:22 -0300

    share

--- src/index.ts
+++ src/index.ts
@@ -69,7 +69,7 @@ fastify.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
         <pre><code>POST /upload</code></pre>
         <p>Send a <code>multipart/form-data</code> request with the file included in the <code>file</code> field.</p>
         <p>Example using <code>curl</code>:</p>
-        <pre><code>curl -F "file=@/path/to/your/file" https://files.andrepaiva.dev/upload</code></pre>
+        <pre><code>curl -F "file=@/path/to/your/file" https://share.andrepaiva.dev/upload</code></pre>
   
         <h2>List Files</h2>
         <p>To list all uploaded files, use the following endpoint:</p>
@@ -78,7 +78,7 @@ fastify.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
         <h2>File Access</h2>
         <p>Files can be accessed directly using the URL provided in the response of the upload endpoint.</p>
         <p>Example URL:</p>
-        <pre><code>https://files.andrepaiva.dev/files/your-file-id.ext</code></pre>
+        <pre><code>https://share.andrepaiva.dev/files/your-file-id.ext</code></pre>
       </body>
       </html>
     `);
